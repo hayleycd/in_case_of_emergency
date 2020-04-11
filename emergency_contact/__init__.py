e9fd12acfd02d9bce4aa61c14ae8716ff9526ab0
Hayley Denbraver <cook.hayley@gmail.com>
2020-04-11 16:36:19 -0700

    refining flow.

--- emergency_contact/__init__.py
+++ emergency_contact/__init__.py
@@ -34,39 +34,69 @@ SAMPLE_INFORMATION = {
         ('Contact F', 'Contact F phone number'),
     ],
     'hire': [
-        ("Hayley is looking for her next opportunity",),
+        ("Hayley Denbraver is looking for her next opportunity",),
         ("Hire her for Developer Advocacy, Technical Content Writing, or Python Development",),
         ('Contact her @hayleydenb on Twitter, or find further information at https://dev.to/hayleydenb'),
     ],
 }
+ACCEPTED_NUMBERS = [TWIL_NUMBER, TWIL_EXAMPLE_NUMBER]
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    emergency_info = SAMPLE_INFORMATION if is_sample(req) else EMERGENCY_INFORMATION
+    
+    # This determines what phone number is texting my twilio number and whether they are texting my sample or emergency number.
+    # I am also getting incoming text body, which will be used to properly respond. 
+    send_to = req.params['From']
+    send_from = req.params['To']
+    incoming_message = req.params['Body'].lower()
 
-    logging.info('Python HTTP trigger function processed a request.')
+    # We run a security check to be sure of the following:
+    # 1. That someone didn't set up a twilio number to hit my site.
+    # 2. That the person texting my number is authorized to do so. We don't want to give health info to a 'wrong number'
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    security_check()
 
-    if name:
-        return func.HttpResponse(f"Hello {name}!")
+    # These variables help construct the proper outgoing messages.
+    
+    is_sample = is_sample(req)
+    emergency_info = SAMPLE_INFORMATION if is_sample else EMERGENCY_INFORMATION
+    outgoing_message = ""
+
+    logging.info(log_incoming_text(is_sample, send_to)
+
+    if incoming_message == "details":
+        for detail in DETAILS:
+            outgoing_message = f"{outgoing_message} {detail} \n"
+    
+    elif incoming_message == "meds":
+        for med in MEDS:
+            outgoing_message = f"{outgoing_message} {med[0]}: {med[1]} \n"
+    
+    elif incoming_message == "contacts":
+        for contact in CONTACTS:
+            outgoing_message = f"{outgoing_message} {contact[0]}: {contact[1]} \n"
+    
     else:
-        return func.HttpResponse(
-             "Please pass a name on the query string or in the request body",
-             status_code=400
+        outgoing_message = """You have requested emergency information for Hayley Denbraver.\n\n"
+        /"- Text 'detail' for medical details like allergies and known conditions.\n\n"
+        /"
+        
+        - Text 'meds' for a list of current medications.
+        
+        - Text 'contacts' for a list of emergency contacts.
+        
+        - Text 'notify' to send Hayley's contacts a message with your phone number."""
+
+    message = client.messages.create(
+        body=outgoing_message,
+        from_=TWIL_NUMBER,
+        to=who_is_this,
         )
 
-def sample_or_real(req):
-    req.params.get['To']
 
-def run_sample_workflow(req):
-    pass
+def is_sample(req):
+    which_number = req.params.get['To']
+    
 
-def run_workflow(req):
-    pass
+def log_incoming_text(is_sample, send_to):
+    sample_or_emergency = 'Sample' if is_sample else 'Emergency'
+    return f'{sample_or_emergency} information was requested from {send_to}'
