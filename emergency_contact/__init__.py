d57abeee041e307e56e38ad33753ebc8d7f48224
Hayley Denbraver <cook.hayley@gmail.com>
2020-04-12 17:16:16 -0700

    Added security stuff

--- emergency_contact/__init__.py
+++ emergency_contact/__init__.py
@@ -10,6 +10,7 @@ AUTH_TOKEN = os.environ['AUTH_TOKEN']
 TWIL_NUMBER = os.environ['TWIL_NUMBER']
 TWIL_EXAMPLE_NUMBER = os.environ['TWIL_EXAMPLE_NUMBER']
 EMERGENCY_PIN = os.environ['EMERGENCY_PIN']
+SAMPLE_PIN = os.environ['SAMPLE_PIN']
 EMERGENCY_INFORMATION = os.environ['EMERGENCY_INFORMATION']
 NAME = os.environ['NAME']
 
@@ -33,10 +34,10 @@ SAMPLE_INFORMATION = {
         ('Contact E', 'Contact E phone number'),
         ('Contact F', 'Contact F phone number'),
     ],
-    'hire': [
+    'shameless': [
         ("Hayley Denbraver is looking for her next opportunity",),
         ("Hire her for Developer Advocacy, Technical Content Writing, or Python Development",),
-        ('Contact her @hayleydenb on Twitter, or find further information at https://dev.to/hayleydenb'),
+        ('Contact her @hayleydenb on Twitter, or find here at https://CodeAndTea.com or https://www.linkedin.com/in/hayleydenbraverpe),
     ],
 }
 ACCEPTED_NUMBERS = [TWIL_NUMBER, TWIL_EXAMPLE_NUMBER]
@@ -44,7 +45,8 @@ ACCEPTED_NUMBERS = [TWIL_NUMBER, TWIL_EXAMPLE_NUMBER]
 def main(req: func.HttpRequest) -> func.HttpResponse:
     
     # This determines what phone number is texting my twilio number and whether they are texting my sample or emergency number.
-    # I am also getting incoming text body, which will be used to properly respond. 
+    # I am also getting the incoming text body, which will be used to properly respond. 
+    
     send_to = req.params['From']
     send_from = req.params['To']
     incoming_message = req.params['Body'].lower()
@@ -53,50 +55,75 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     # 1. That someone didn't set up a twilio number to hit my site.
     # 2. That the person texting my number is authorized to do so. We don't want to give health info to a 'wrong number'
 
-    security_check()
+    security_check(send_to, send_from, incoming_message)
 
     # These variables help construct the proper outgoing messages.
     
     is_sample = is_sample(req)
     emergency_info = SAMPLE_INFORMATION if is_sample else EMERGENCY_INFORMATION
-    outgoing_message = ""
+
+    # This logs incoming text. 
 
     logging.info(log_incoming_text(is_sample, send_to)
 
-    if incoming_message == "details":
-        for detail in DETAILS:
-            outgoing_message = f"{outgoing_message} {detail} \n"
-    
-    elif incoming_message == "meds":
-        for med in MEDS:
-            outgoing_message = f"{outgoing_message} {med[0]}: {med[1]} \n"
-    
-    elif incoming_message == "contacts":
-        for contact in CONTACTS:
-            outgoing_message = f"{outgoing_message} {contact[0]}: {contact[1]} \n"
-    
-    else:
-        outgoing_message = """You have requested emergency information for Hayley Denbraver.\n\n"
-        /"- Text 'detail' for medical details like allergies and known conditions.\n\n"
-        /"
-        
-        - Text 'meds' for a list of current medications.
-        
-        - Text 'contacts' for a list of emergency contacts.
-        
-        - Text 'notify' to send Hayley's contacts a message with your phone number."""
 
-    message = client.messages.create(
-        body=outgoing_message,
-        from_=TWIL_NUMBER,
-        to=who_is_this,
-        )
 
 
 def is_sample(req):
     which_number = req.params.get['To']
+    if which_number == TWIL_EXAMPLE_NUMBER:
+        return True
+    elif which_number == TWIL_NUMBER:
+        return False
+    else:
+        return None
     
 
 def log_incoming_text(is_sample, send_to):
     sample_or_emergency = 'Sample' if is_sample else 'Emergency'
     return f'{sample_or_emergency} information was requested from {send_to}'
+
+def security_check(req, send_to, send_from, incoming_message):
+    is_sample = is_sample(req)
+    
+    if is_sample == None:
+        logging.info(f"Request was sent to an unauthorized number {send_to}")
+        return func.HttpResponse(f"The request was sent to an unauthorized number")
+
+    PIN = SAMPLE_PIN if is_sample else EMERGENCY_PIN
+    body = incoming_message.strip().lower()
+
+    if body == PIN:
+        send_initial_text(req, send_to, send_from, is_sample)
+    
+    else:
+        messages = client.messages.list(
+            from_=send_to,
+            body=PIN
+        )
+        if messages:
+            send_follow_up_text(req, send_to, send_from, incoming_message)
+        
+        else:
+            return func.HttpResponse(f"See ID for instructions on texting")
+
+def send_initial_text(req, send_to, send_from, is_sample):
+    outgoing_message = f"""You have requested {NAME}'s emergency information.
+        - Text 'contacts' for {NAME}'s emergency contacts
+        - Text 'meds' for a list of {NAME}'s medications
+        - Text 'details' for information like blood type, known conditions, etc.""" 
+
+    if is_sample:
+        outgoing_message = outgoing_message + "\n- Text 'shameless' for details on how to hire Hayley."
+    
+    message = client.messages.create(
+        body=outgoing_message,
+        from_=send_from,
+        to=send_to,
+        )
+
+def send_follow_up_text(req, send_to, send_from, body, emergency_info):
+    
+
+
+    
